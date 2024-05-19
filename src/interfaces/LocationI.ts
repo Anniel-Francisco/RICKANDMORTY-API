9a39c2b410d91e7dee7f770128b20f29a3fa441f
Anniel <annielfranciscoreyes@gmail.com>
2024-05-19 05:57:10 -0400

    style(rickandmorty-api): formatting code

--- src/interfaces/LocationI.ts
+++ src/interfaces/LocationI.ts
@@ -1,9 +1,9 @@
 interface LocationI{
-    id:number;
-    name:string;
-    type:string;
-    dimension:string;
-    residents:Array<string>;
+    id: number;
+    name: string;
+    type: string;
+    dimension: string;
+    residents: Array<string>;
 }
 
 export default LocationI;
