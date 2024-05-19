23501ea9ccc77723e8f91c47bb2bb1c523230c58
Anniel <annielfranciscoreyes@gmail.com>
2024-05-19 11:19:34 -0400

    style(rickandmorty-api): formatting code in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,52 @@
-import {ReactNode} from 'react';
+import { ReactNode } from "react";
 //
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 
 type HomeT = {
-   icon: ReactNode,
-   link:string,
-}
-export default function Home(){
+  icon: ReactNode;
+  link: string;
+};
+export default function Home() {
+  const icons: Array<HomeT> = [
+    {
+      icon: <FaGithub size={30} className="text-white" />,
+      link: "https://github.com/Anniel-Francisco/RICKANDMORTY-API.git",
+    },
+    {
+      icon: <FaLinkedin size={30} className="text-white" />,
+      link: "https://www.linkedin.com/in/anniel-francisco-reyes-javier-b22347274/",
+    },
+  ];
 
-          const icons:Array<HomeT> = [
-                    {icon: <FaGithub size={30} className="text-white" />, link:'https://github.com/Anniel-Francisco/RICKANDMORTY-API.git'},
-                    {icon: <FaLinkedin size={30} className="text-white"/>, link:'https://www.linkedin.com/in/anniel-francisco-reyes-javier-b22347274/'},
-          ];
+  return (
+    <div
+      style={{
+        backgroundColor: "#262c3a",
+        height: "calc(100vh - 80px)",
+        backgroundPositionY: "75%",
+        padding: "10px 20px",
+      }}
+      className="w-full flex max-md:flex-wrap justify-start text-8xl font-extrabold bg-[url('src/assets/background-rick.jpg')] bg-no-repeat bg-cover"
+    >
+      <div className="flex items-center">
+        <h1 className="text-white max-md:text-center">Rick and Morty API</h1>
+      </div>
 
-          return(
-               <div style={{backgroundColor:"#262c3a", height:"calc(100vh - 80px)", backgroundPositionY:'75%', padding:'10px 20px'}} className="w-full flex max-md:flex-wrap justify-start text-8xl font-extrabold bg-[url('src/assets/background-rick.jpg')] bg-no-repeat bg-cover">
-                   <div className="flex items-center">
-                    <h1 className="text-white max-md:text-center">Rick and Morty API</h1>
-                   </div>
-
-                   <div className="flex items-end justify-end w-full gap-3">
-                    {
-                              icons.map((icon,index) =>{
-                                     return( 
-                                        <a key={index} href={icon.link} target="_blank" className="hover:-translate-y-1 duration-300">
-                                                  {icon.icon}
-                                        </a>); 
-                              })
-                    }
-                   </div>
-                </div>        
+      <div className="flex items-end justify-end w-full gap-3">
+        {icons.map((icon, index) => {
+          return (
+            <a
+              key={index}
+              href={icon.link}
+              target="_blank"
+              className="hover:-translate-y-1 duration-300"
+            >
+              {icon.icon}
+            </a>
           );
+        })}
+      </div>
+    </div>
+  );
 }
