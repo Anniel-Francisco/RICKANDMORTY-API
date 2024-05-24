505d5bdcb7bed2391b54ae9136fd94c2f6d1c958
Anniel-Reyes <areyes@brisiao.do>
2024-05-24 19:17:24 -0400

    style(rickandmorty-api): fixing warnings

--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -21,37 +21,38 @@ export default function Locations() {
     count: 0
   });
 
-  const getLocations = async () => {
-    try {
-      if (!location.search) location.search = `?page=1`;
-      const {
-        data: {
-          info: { pages },
-          results,
-        },
-      } = await RickMortyAPI.getLocations(location.search);
-      setPagination({
-        ...pagination,
-        count: pages
-      });
-      setLocations(results);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const setCurrentPage = () => {
-    const searchParams = new URLSearchParams(location.search);
-    pagination.page = parseInt(searchParams.get("page") || "");
-  };
-
+  
   useEffect(() => {
+    const getLocations = async () => {
+      try {
+        if (!location.search) location.search = `?page=1`;
+        const {
+          data: {
+            info: { pages },
+            results,
+          },
+        } = await RickMortyAPI.getLocations(location.search);
+        setPagination({
+          ...pagination,
+          count: pages
+        });
+        setLocations(results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const setCurrentPage = () => {
+      const searchParams = new URLSearchParams(location.search);
+      pagination.page = parseInt(searchParams.get("page") || "");
+    };
+    //
     getLocations();
     setCurrentPage();
-  }, [location.search]);
+  }, [location, location.search, pagination]);
 
   useEffect(() => {
     navigate(`${location.pathname}${location.search}`);
-  }, []);
+  }, [location.pathname, location.search, navigate]);
   return (
     <div
       style={{
